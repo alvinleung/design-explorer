ffbbe9871f68d44ba6bdd1e4e81b4183c530900e
Alvin <alvinleung2009@gmail.com>
2020-08-07 00:07:59 -0700

    Implemented search for multiple components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+// import "./index.css";
+// import App from "./App";
 import DesignExplorer from "./components/designExplorer";
-import * as serviceWorker from "./serviceWorker";
+// import * as serviceWorker from "./serviceWorker";
 
 const imgList = [
   `${process.env.PUBLIC_URL}/img/8 Viewing your tasks.svg`,
@@ -15,14 +15,25 @@ const imgList = [
   `${process.env.PUBLIC_URL}/img/14 Leaving Reviews.svg`,
 ];
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
+
+// grab all the viewer elements on html
+window.addEventListener("load", function () {
+  const designExplorerList = document.querySelectorAll(".design-explorer");
+  designExplorerList.forEach((elm) => {
+    const srcListAttribute = elm.getAttribute("data-src").replaceAll("'", '"');
+    const srcList = JSON.parse(srcListAttribute);
+    console.log("test");
+    ReactDOM.render(<DesignExplorer src={srcList} cols={3} />, elm);
+  });
+});
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
